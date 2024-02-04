4a21fec5ee6bd587e117028b22eef02a9cf934bd
Kjaer <kjaerkjaer@protonmail.com>
2024-02-04 14:21:45 +0100

    REFACTOR code for loading parsers

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ from parsers.lane import Lane
 from parsers.region import Region
 from parsers.releaseDate import ReleaseDate
 
-champions = Champions("14.2.1").parse()
-champions = ReleaseDate(champions).parse()
-champions = Region(champions).parse()
-champions = Lane(champions).parse()
+def main():
+    champions_version = "14.2.1"
+    
+    champions_data = Champions(champions_version).parse()
+    champions_data = ReleaseDate(champions_data).parse()
+    champions_data = Region(champions_data).parse()
+    champions_data = Lane(champions_data).parse()
 
-output_file_path = "champions.json"
-with open(output_file_path, "w") as json_file:
-    json.dump(champions, json_file, sort_keys=False, indent=4)
+    output_file_path = "champions.json"
+    save_data_to_json(output_file_path, champions_data)
 
-print(f"Champion data has been saved to {output_file_path}")
+    print(f"Champion data has been saved to {output_file_path}")
+
+def save_data_to_json(file_path, data):
+    with open(file_path, "w") as json_file:
+        json.dump(data, json_file, sort_keys=False, indent=4)
+
+if __name__ == "__main__":
+    main()
